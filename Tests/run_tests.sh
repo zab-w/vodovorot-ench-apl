5d5371cafaaf21b5e3c3e9a7265ac7b1faa03d43
zab-w <hz121234hz@gmail.com>
2021-06-24 13:06:52 +0300

    Use apl

--- Tests/run_tests.sh
+++ Tests/run_tests.sh
@@ -3,5 +3,6 @@
 find /app/SimulationCraft/simc
 find /apl/Tests/boris.simc
 mkdir /html
-/app/SimulationCraft/simc /apl/Tests/boris.simc desired_targets=3 html=/html/boris_3_targets.html
-/app/SimulationCraft/simc /apl/Tests/boris.simc desired_targets=4 html=/html/boris_4_targets.html
+/app/SimulationCraft/simc /apl/Tests/boris.simc /apl/elem-apl.simc desired_targets=3 html=/html/boris_3_targets.html
+/app/SimulationCraft/simc /apl/Tests/boris.simc /apl/elem-apl.simc desired_targets=4 html=/html/boris_4_targets.html
+/app/SimulationCraft/simc /apl/Tests/boris.simc /apl/elem-apl.simc desired_targets=5 html=/html/boris_5_targets.html
