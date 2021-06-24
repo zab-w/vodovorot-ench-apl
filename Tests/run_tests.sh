ae54e160d10ae0d6cec86bed00e6b98998b25038
zab-w <hz121234hz@gmail.com>
2021-06-24 11:45:05 +0300

    Missing header for sh

--- Tests/run_tests.sh
+++ Tests/run_tests.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 /app/SimulationCraft/simc /apl/Tests/boris.simc desired_targets=3 html=/html/boris_3_targets.html
 /app/SimulationCraft/simc /apl/Tests/boris.simc desired_targets=4 html=/html/boris_4_targets.html
