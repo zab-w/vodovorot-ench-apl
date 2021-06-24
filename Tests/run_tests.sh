9e8c86d296f74ff468a75399a1b26f7108132a28
zab-w <hz121234hz@gmail.com>
2021-06-24 11:33:43 +0300

    Add run_tests.sh

---
+++ Tests/run_tests.sh
@@ -0,0 +1,2 @@
+/app/SimulationCraft/simc /apl/Tests/boris.simc desired_targets=3 html=/html/boris_3_targets.html
+/app/SimulationCraft/simc /apl/Tests/boris.simc desired_targets=4 html=/html/boris_4_targets.html
