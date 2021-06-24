b70e6638e19022c5336a6fcf61574c1ece8483d5
zab-w <hz121234hz@gmail.com>
2021-06-24 11:47:19 +0300

    Test for missing files

--- Tests/run_tests.sh
+++ Tests/run_tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+find /app/SimulationCraft/simc
+find /apl/Tests/boris.simc
+mkdir /html
 /app/SimulationCraft/simc /apl/Tests/boris.simc desired_targets=3 html=/html/boris_3_targets.html
 /app/SimulationCraft/simc /apl/Tests/boris.simc desired_targets=4 html=/html/boris_4_targets.html
