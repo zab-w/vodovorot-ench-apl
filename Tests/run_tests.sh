f527b7c3b785b2d73c15382929d6ba3788e0a4ab
zab-w <hz121234hz@gmail.com>
2021-06-29 19:23:00 +0300

    Rework run_tests.sh

--- Tests/run_tests.sh
+++ Tests/run_tests.sh
@@ -3,6 +3,20 @@
 find /app/SimulationCraft/simc
 find /apl/Tests/boris.simc
 mkdir /html
-/app/SimulationCraft/simc /apl/Tests/boris.simc /apl/elem-apl.simc desired_targets=3 html=/html/boris_3_targets.html
-/app/SimulationCraft/simc /apl/Tests/boris.simc /apl/elem-apl.simc desired_targets=4 html=/html/boris_4_targets.html
-/app/SimulationCraft/simc /apl/Tests/boris.simc /apl/elem-apl.simc desired_targets=5 html=/html/boris_5_targets.html
+SIMC=/app/SimulationCraft/simc
+TEST_PROFILES_FOLDER=/apl/Tests/test_profiles
+APL_PATH=/apl/elem-apl.simc
+
+TEST_APLS=('eogs_mote_fae','eogs_mote_necro','eogs_se_fae','eogs_se_necro','skyb_mote_fae','skyb_mote_necro','skyb_se_fae','skyb_se_necro')
+PARAMS=''
+for apl_name in "${TEST_APLS[@]}"
+do
+	PARAMS=$PARAMS $TEST_PROFILES_FOLDER/$apl_name.simc $APL_PATH name=$apl_name_apl $TEST_PROFILES_FOLDER/$apl_name.simc name=$apl_name_baseline
+done
+
+$SIMC $PARAMS desired_targets=1 html=/html/1_targets.html
+$SIMC $PARAMS desired_targets=2 html=/html/2_targets.html
+$SIMC $PARAMS desired_targets=3 html=/html/3_targets.html
+$SIMC $PARAMS desired_targets=4 html=/html/4_targets.html
+$SIMC $PARAMS desired_targets=5 html=/html/5_targets.html
+$SIMC $PARAMS desired_targets=10 html=/html/10_targets.html
